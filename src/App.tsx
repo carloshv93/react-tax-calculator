f75071525cfb90d5dcfd02e95190c499fa08d080
carlos <carloshv93@gmail.com>
2022-03-20 13:45:56 -0600

    feat: add Container styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  align-items: center;
+`
 
 function App () {
+  return (
+    <Container>
+    </Container>
+  )
 }
 
 export default App
