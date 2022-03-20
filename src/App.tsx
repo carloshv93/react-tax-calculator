3085e78a30b36865ab53865f0708e2d663bdff9c
carlos <carloshv93@gmail.com>
2022-03-20 15:09:28 -0600

    feat: display intl formated balance value

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,30 @@ import Input from './components/Input'
 import Button from './components/Button'
 import Container from './components/Container'
 import Section from './components/Section'
+import Balance from './components/Balance'
+import { useState } from 'react'
 
+const compoundInterest = (deposit:number, contribution:number, years:number, rate:number) => {
+  let total:number = deposit
+  for (let i = 0; i < years; i++) {
+    total += contribution
+    total *= rate + 1
+  }
+  return Math.round(total)
+}
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2
+})
 function App () {
-  const handleSubmit = () => {}
+  const [balance, setBalace] = useState(0)
+  const handleSubmit = ({ deposit, contribution, years, rate }) => {
+    const value:number = compoundInterest(Number(deposit), Number(contribution), Number(years), Number(rate))
+    setBalace(value)
+  }
+
   return (
     <Container>
       <Section>
@@ -23,9 +44,10 @@ function App () {
             <Input name="contribution" label="Contribution"/>
             <Input name="years" label="Years"/>
             <Input name="rate" label="Rate"/>
-            <Button>Calculate</Button>
+            <Button type='submit'>Calculate</Button>
           </Form>
         </Formik>
+          {balance > 0 && <Balance>Final balance: {formatter.format(balance)}</Balance>}
       </Section>
     </Container>
   )
